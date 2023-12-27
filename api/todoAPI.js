8cf8081e172e228dc5ae2d6218355213448111c4
Zojo24 <hchoi2894@gmail.com>
2023-12-27 23:21:25 +0900

    API export/import 작업

--- api/todoAPI.js
+++ api/todoAPI.js
@@ -1,69 +1,78 @@
-export default class TodoAPI {
-	//POST//
-	async function createTodo() {
-		const res = await fetch('https://asia-northeast3-heropy-api.cloudfunctions.net/api/todos', {
-			method: 'POST',
-			headers: {
-				'content-type': 'application/json',
-				'apikey': 'KDT7_GrZ1eYBo',
-				'username': 'KDT7_ChoiHongJoo'
-			},
-			body: JSON.stringify({
-			})
-		})
+//POST//
+export class CRUD {
+	async createTodo() {
+		const res = await fetch(
+			'https://asia-northeast3-heropy-api.cloudfunctions.net/api/todos',
+			{
+				method: 'POST',
+				headers: {
+					'content-type': 'application/json',
+					apikey: 'KDT7_GrZ1eYBo',
+					username: 'KDT7_ChoiHongJoo'
+				},
+				body: JSON.stringify({})
+			}
+		)
 		const json = await res.json()
 		console.log(json)
-	
+
 		return json
 	}
-
 	//GET//
-	async function readTodo() {
-		const res = await fetch('https://asia-northeast3-heropy-api.cloudfunctions.net/api/todos', {
-			method: 'GET',
-			headers: {
-				'content-type': 'application/json',
-				'apikey': 'KDT7_GrZ1eYBo',
-				'username': 'KDT7_ChoiHongJoo'
+	async readTodo() {
+		const res = await fetch(
+			'https://asia-northeast3-heropy-api.cloudfunctions.net/api/todos',
+			{
+				method: 'GET',
+				headers: {
+					'content-type': 'application/json',
+					apikey: 'KDT7_GrZ1eYBo',
+					username: 'KDT7_ChoiHongJoo'
+				}
 			}
-		})
+		)
 		const json = await res.json()
 		console.log(json)
-	
+
 		return json
 	}
 
 	//PUT//
-	async function updateTodo() {
-		const res = await fetch('https://asia-northeast3-heropy-api.cloudfunctions.net/api/todos/:todoId', {
-			method: 'PUT',
-			headers: {
-				'content-type': 'application/json',
-				'apikey': 'KDT7_GrZ1eYBo',
-				'username': 'KDT7_ChoiHongJoo'
-			},
-			body: JSON.stringify({
-			})
-		})
+	async updateTodo(todoId, updateData) {
+		const res = await fetch(
+			'https://asia-northeast3-heropy-api.cloudfunctions.net/api/todos/:todoId',
+			{
+				method: 'PUT',
+				headers: {
+					'content-type': 'application/json',
+					apikey: 'KDT7_GrZ1eYBo',
+					username: 'KDT7_ChoiHongJoo'
+				},
+				body: JSON.stringify(updateData)
+			}
+		)
 		const json = await res.json()
 		console.log(json)
-	
+
 		return json
 	}
 
 	//DELETE//
-	async function deleteTodo() {
-		const res = await fetch('https://asia-northeast3-heropy-api.cloudfunctions.net/api/todos/:todoId', {
-			method: 'DELETE',
-			headers: {
-				'content-type': 'application/json',
-				'apikey': 'KDT7_GrZ1eYBo',
-				'username': 'KDT7_ChoiHongJoo'
-			},
-		})
+	async deleteTodo(todoId) {
+		const res = await fetch(
+			'https://asia-northeast3-heropy-api.cloudfunctions.net/api/todos/:todoId',
+			{
+				method: 'DELETE',
+				headers: {
+					'content-type': 'application/json',
+					apikey: 'KDT7_GrZ1eYBo',
+					username: 'KDT7_ChoiHongJoo'
+				}
+			}
+		)
 		const json = await res.json()
 		console.log(json)
-	
+
 		return json
 	}
 }
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,4 +1,5 @@
 import { Component } from '../core/zojo'
+// import CRUD from '/api/todoAPI'
 
 export default class TodoList extends Component {
 	constructor(props) {
@@ -7,63 +8,41 @@ export default class TodoList extends Component {
 			taskItems: []
 		}
 	}
-
+	async createTodo() {
+		console.log('Creating Todo List')
+	}
 	async render() {
 		this.el.classList.add('todo-item')
 		this.el.innerHTML = /*html*/ `
-      <div class="type">
-        <span>작업 유형</span>
-        <input/>
-      </div>
+    <div class="items">
+      <button class="add" onclick="createTodo()">+</button>
 
       <div class="task">
-        <span>작업 내용</span>
-        <input placeholder="해야하는 작업을 작성해주세요."/>
-      </div>
+          <span>작업 내용</span>
+          <input placeholder="해야하는 작업을 작성해주세요."/>
+        </div>
 
-      <div class="start-date">
-        <span>시작일</span>
-        <input type="date">
-      </div>
+        <div class="due-date">
+          <span>마감일</span>
+          <input type="date">
+        </div>
 
-      <div class="due-date">
-        <span>마감일</span>
-        <input type="date">
-      </div>
+        <div class="status">
+          <span>진행 상황</span>
+          <select>
+            <option value="todo">진행 전</option>
+            <option value="in-progress">진행 중</option>
+            <option value="completed">진행 완료</option>
+          </select>
+        </div>
 
-      <div class="status">
-        <span>진행 상황</span>
-        <select>
-          <option value="todo">진행 전</option>
-          <option value="in-progress">진행 중</option>
-          <option value="completed">진행 완료</option>
-        </select>
-      </div>
-
-      <div class="output">
-        <span>결과물</span>
-        <input placeholder="결과물에 대해 간략하게 작성해주세요."/>
-      </div>
-
-      <div class="follow-up">
-        <span>후속 작업</span>
-        <input placeholder="후속 작업을 작성해주세요."/>
-      </div>
-
-      <div class="manager">
-        <span>결재자</span>
-        <input/>
-      </div>
+        <div class="manager">
+          <span>결재자</span>
+          <input/>
+        </div>
+    </div>
     `
-
-		const taskItemsEl = this.el.querySelector('.taskItems')
-
-		if (Array.isArray(this.state.taskItems)) {
-			for (const taskItem of this.state.taskItems) {
-				const todoItem = new TodoItem({ taskItem })
-				await todoItem.render()
-				taskItemsEl.append(todoItem.el)
-			}
-		}
+		const addButton = this.el.querySelector('.add')
+		addButton.addEventListener('click', () => this.createTodo())
 	}
 }
