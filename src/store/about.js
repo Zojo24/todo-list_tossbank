5e129c5dd80c07e6e885ff78134fa46b018326ff
Zojo24 <hchoi2894@gmail.com>
2023-12-27 18:37:11 +0900

    API-POST 코드 수정

--- src/store/about.js
+++ src/store/about.js
@@ -1,7 +1,7 @@
 import { Store } from '../core/zojo'
 
 export default new Store({
-	photo: 'https://heropy.blog.css/images/logo.png',
+	photo: '',
 	name: 'Zojo / Hong Joo Choi',
 	email: 'hchoi2894@gmail.com',
 	blog: 'https://velog.io/@zojo24/'
