3cecba1cb4cee77ee021d5a0a9fc12386d4835d6
Zojo24 <hchoi2894@gmail.com>
2023-12-27 11:27:02 +0900

    Store 컴포넌트 정의

--- src/core/zojo.js
+++ src/core/zojo.js
@@ -43,3 +43,29 @@ export function createRouter(routes) {
 		routerRender(routes)
 	}
 }
+
+//store//
+export class Store {
+	constructor(state) {
+		this.state = {}
+		this.observers = {}
+		for (const key in state) {
+			//상태 변경 감시 설정//
+			Object.defineProperty(this.state, key, {
+				get: () => state[key],
+				set: val => {
+					state[key] = val
+					if (Array.isArray(this.observers[key])) {
+						this.observers[key].forEach(observer => observer(val))
+					}
+				}
+			})
+		}
+	}
+	// 상태 변경 구독//
+	subscribe(key, cb) {
+		Array.isArray(this.observers[key])
+			? this.observers[key].push(cb)
+			: (this.observers[key] = [cb])
+	}
+}
