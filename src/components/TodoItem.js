0c0aa0e155088795610ea34518f3846929af6dcf
Zojo24 <hchoi2894@gmail.com>
2023-12-27 23:32:05 +0900

    TodoItem.js 생성

---
+++ src/components/TodoItem.js
@@ -0,0 +1,41 @@
+import { Component } from '../core/zojo'
+
+export class TodoItem extends Component {
+	constructor(props) {
+		super(props)
+	}
+	async render() {
+		this.el.classList.add('todo-item')
+		this.el.innerHTML = /*html*/ `
+    <div class="items">
+      <button class="add" onclick="createTodo()">+</button>
+  
+      <div class="task">
+          <span>작업 내용</span>
+          <input placeholder="해야하는 작업을 작성해주세요."/>
+        </div>
+  
+        <div class="due-date">
+          <span>마감일</span>
+          <input type="date">
+        </div>
+  
+        <div class="status">
+          <span>진행 상황</span>
+          <select>
+            <option value="todo">진행 전</option>
+            <option value="in-progress">진행 중</option>
+            <option value="completed">진행 완료</option>
+          </select>
+        </div>
+  
+        <div class="manager">
+          <span>결재자</span>
+          <input/>
+        </div>
+    </div>
+    `
+		const addButton = this.el.querySelector('.add')
+		addButton.addEventListener('click', () => this.createTodo())
+	}
+}
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,4 +1,5 @@
 import { Component } from '../core/zojo'
+import TodoItem from './TodoItem'
 // import CRUD from '/api/todoAPI'
 
 export default class TodoList extends Component {
@@ -13,36 +14,14 @@ export default class TodoList extends Component {
 	}
 	async render() {
 		this.el.classList.add('todo-item')
-		this.el.innerHTML = /*html*/ `
-    <div class="items">
-      <button class="add" onclick="createTodo()">+</button>
+  }
+  const taskItemsEl = this.el.querySelector('.taskItems')
 
-      <div class="task">
-          <span>작업 내용</span>
-          <input placeholder="해야하는 작업을 작성해주세요."/>
-        </div>
-
-        <div class="due-date">
-          <span>마감일</span>
-          <input type="date">
-        </div>
-
-        <div class="status">
-          <span>진행 상황</span>
-          <select>
-            <option value="todo">진행 전</option>
-            <option value="in-progress">진행 중</option>
-            <option value="completed">진행 완료</option>
-          </select>
-        </div>
-
-        <div class="manager">
-          <span>결재자</span>
-          <input/>
-        </div>
-    </div>
-    `
-		const addButton = this.el.querySelector('.add')
-		addButton.addEventListener('click', () => this.createTodo())
-	}
+  if (Array.isArray(this.state.taskItems)) {
+    for (const taskItem of this.state.taskItems) {
+      const todoItem = new todoItem({ taskItem })
+      await todoItem.render()
+      taskItemsEl.append(todoItem.el)
+    }
+  }
 }
