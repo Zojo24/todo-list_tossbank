35f5bc7f753a9daccc562b032f43707a5f447fee
Zojo24 <hchoi2894@gmail.com>
2023-12-27 21:59:21 +0900

    REST API 설정

--- api/todoAPI.js
+++ api/todoAPI.js
@@ -1,25 +1,69 @@
 export default class TodoAPI {
 	//POST//
-	async createTodo() {
-		const requestBody = {
-			title: 'Todo List',
-			order: 1
-		}
-		const res = await fetch(
-			'https://asia-northeast3-heropy-api.cloudfunctions.net/api/todos',
-			{
-				method: 'POST',
-				headers: {
-					'content-type': 'application/json',
-					apikey: 'KDT7_GrZ1eYBo',
-					username: 'KDT7_ChoiHongJoo'
-				},
-				body: JSON.stringify(requestBody)
-			}
-		)
+	async function createTodo() {
+		const res = await fetch('https://asia-northeast3-heropy-api.cloudfunctions.net/api/todos', {
+			method: 'POST',
+			headers: {
+				'content-type': 'application/json',
+				'apikey': 'KDT7_GrZ1eYBo',
+				'username': 'KDT7_ChoiHongJoo'
+			},
+			body: JSON.stringify({
+			})
+		})
 		const json = await res.json()
 		console.log(json)
+	
+		return json
+	}
 
+	//GET//
+	async function readTodo() {
+		const res = await fetch('https://asia-northeast3-heropy-api.cloudfunctions.net/api/todos', {
+			method: 'GET',
+			headers: {
+				'content-type': 'application/json',
+				'apikey': 'KDT7_GrZ1eYBo',
+				'username': 'KDT7_ChoiHongJoo'
+			}
+		})
+		const json = await res.json()
+		console.log(json)
+	
+		return json
+	}
+
+	//PUT//
+	async function updateTodo() {
+		const res = await fetch('https://asia-northeast3-heropy-api.cloudfunctions.net/api/todos/:todoId', {
+			method: 'PUT',
+			headers: {
+				'content-type': 'application/json',
+				'apikey': 'KDT7_GrZ1eYBo',
+				'username': 'KDT7_ChoiHongJoo'
+			},
+			body: JSON.stringify({
+			})
+		})
+		const json = await res.json()
+		console.log(json)
+	
+		return json
+	}
+
+	//DELETE//
+	async function deleteTodo() {
+		const res = await fetch('https://asia-northeast3-heropy-api.cloudfunctions.net/api/todos/:todoId', {
+			method: 'DELETE',
+			headers: {
+				'content-type': 'application/json',
+				'apikey': 'KDT7_GrZ1eYBo',
+				'username': 'KDT7_ChoiHongJoo'
+			},
+		})
+		const json = await res.json()
+		console.log(json)
+	
 		return json
 	}
 }
