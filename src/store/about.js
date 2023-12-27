99b4bfeccb614a437caf654461b0a63711d80988
Zojo24 <hchoi2894@gmail.com>
2023-12-27 11:01:49 +0900

    About 정보를 담은 Store 생성

---
+++ src/store/about.js
@@ -0,0 +1,8 @@
+import { Store } from '../core/zojo'
+
+export default new Store({
+	photo: '',
+	name: 'Zojo / Hong Joo Choi',
+	email: 'hchoi2894@gmail.com',
+	blog: 'https://velog.io/@zojo24/'
+})
