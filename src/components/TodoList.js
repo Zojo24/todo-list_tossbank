8fed1e99b03683c3b4d6f8112a6346e7118c7e5d
Zojo24 <hchoi2894@gmail.com>
2023-12-28 22:50:12 +0900

    GET, DELETE 작업

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,26 +1,121 @@
 import { Component } from '../core/zojo'
-import TodoItem from './TodoItem'
+import CRUD from '/api/todoAPI'
 
 export default class TodoList extends Component {
-	constructor() {
-		super()
+	constructor(props) {
+		super(props)
+		this.crud = new CRUD()
 	}
-	render() {
-		this.el.classList.add('todo-list')
-		this.el.innerHTML = /* html */ `
-			<ul class="todo-items"></ul>
-		`
+	//API POST//
+	async createTodo(taskInput, dateInput) {
+		const parameter = taskInput.value + '##' + dateInput.value
 
-		const todoItemsEl = this.el.querySelector('.todo-items')
-		const todos = [
+		const res = await fetch(
+			'https://asia-northeast3-heropy-api.cloudfunctions.net/api/todos',
 			{
-				title: '',
-				order: ''
+				method: 'POST',
+				headers: {
+					'content-type': 'application/json',
+					apikey: 'KDT7_GrZ1eYBo',
+					username: 'KDT7_ChoiHongJoo'
+				},
+				body: JSON.stringify({
+					title: parameter,
+					order: ''
+				})
 			}
-		]
-		todos.forEach(todo => {
-			const todoItem = new TodoItem(todo)
-			todoItemsEl.append(todoItem.el)
-		})
+		)
+		const json = await res.json()
+		console.log(json)
+
+		return json
+	}
+
+	//GET//
+	async readTodo() {
+		console.log('readTodo')
+		const res = await fetch(
+			'https://asia-northeast3-heropy-api.cloudfunctions.net/api/todos',
+			{
+				method: 'GET',
+				headers: {
+					'content-type': 'application/json',
+					apikey: 'KDT7_GrZ1eYBo',
+					username: 'KDT7_ChoiHongJoo'
+				}
+			}
+		)
+			.then(response => {
+				if (!response.ok) {
+					throw new Error(`HTTP error! Status: ${response.status}`)
+				}
+				return response.json()
+			})
+			.then(data => console.log(data))
+			.catch(error => console.error('Fetch Error:', error))
+	}
+
+	//API DELETE//
+	async deleteTodo(todoId) {
+		const res = await fetch(
+			'https://asia-northeast3-heropy-api.cloudfunctions.net/api/todos/:todoId',
+			{
+				method: 'DELETE',
+				headers: {
+					'content-type': 'application/json',
+					apikey: 'KDT7_GrZ1eYBo',
+					username: 'KDT7_ChoiHongJoo'
+				}
+			}
+		)
+		const json = await res.json()
+		console.log(json)
+
+		return json
+	}
+	//todoItem//
+	async render() {
+		this.el.classList.add('todo-item')
+		this.el.innerHTML = /*html*/ `
+        <div class="items">
+            <div class="task">
+                <span>작업 내용</span>
+                <input class="task-input" placeholder="해야하는 작업을 작성해주세요."/>
+            </div>
+      
+            <div class="due-date">
+                <span>마감일</span>
+                <input class="date-input" type="date">
+            </div>
+      
+            <div class="status">
+                <span>진행 상황</span>
+                <select>
+                    <option value="todo">진행 전</option>
+                    <option value="in-progress">진행 중</option>
+                    <option value="completed">진행 완료</option>
+                </select>
+            </div>
+      
+            <div class="manager">
+                <span>결재자</span>
+                <input/>
+            </div>
+            <button class="add">+</button>
+            <button class="delete">-</button>
+        </div>
+        `
+		window.addEventListener('load', () => this.readTodo())
+
+		const taskInput = this.el.querySelector('.task-input')
+		const dateInput = this.el.querySelector('.date-input')
+
+		const addButton = this.el.querySelector('.add')
+		addButton.addEventListener('click', () =>
+			this.createTodo(taskInput, dateInput)
+		)
+
+		const deleteButton = this.el.querySelector('.delete')
+		deleteButton.addEventListener('click', () => this.deleteTodo())
 	}
 }
