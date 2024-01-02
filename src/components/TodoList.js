cbd1b121409ffe17adda915c620e1b1a758ddafc
Zojo24 <hchoi2894@gmail.com>
2024-01-02 15:30:05 +0900

    항목을 수정할 수 있도록 span -> input 변경

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -7,7 +7,7 @@ export default class TodoList extends Component {
 		this.crud = new CRUD()
 	}
 	//API POST//
-	async createTodo(taskInput, dateInput, managerInput) {
+	async createTodo(taskInput, dateInput) {
 		const parameter = taskInput.value + '##' + dateInput.value
 
 		const res = await fetch(
@@ -34,6 +34,37 @@ export default class TodoList extends Component {
 			.then(() => {
 				window.location.reload()
 			})
+			.catch(error => console.error('Fetch Error:', error))
+	}
+
+	//API PUT//
+	readTodo(taskInput, dateInput, statusInput) {
+		const parameter =
+			taskInput.value + '##' + dateInput.value + '##' + statusInput.value
+		console.log(parameter)
+
+		const res = fetch(
+			'https://asia-northeast3-heropy-api.cloudfunctions.net/api/todos/:todoId',
+			{
+				method: 'PUT',
+				headers: {
+					'content-type': 'application/json',
+					apikey: 'KDT7_GrZ1eYBo',
+					username: 'KDT7_ChoiHongJoo'
+				},
+				body: JSON.stringify({
+					title: parameter,
+					done: ''
+				})
+			}
+		)
+			.then(response => {
+				if (!response.ok) {
+					throw new Error(`HTTP error! Status: ${response.status}`)
+				}
+				return response.json()
+			})
+			.then(data => this.getTodoList(data))
 			.catch(error => console.error('Fetch Error:', error))
 	}
 
@@ -64,7 +95,7 @@ export default class TodoList extends Component {
 			.catch(error => console.error('Fetch Error:', error))
 	}
 
-	//GET//
+	//API GET//
 	readTodo() {
 		const res = fetch(
 			'https://asia-northeast3-heropy-api.cloudfunctions.net/api/todos',
@@ -93,15 +124,15 @@ export default class TodoList extends Component {
 			this.el.innerHTML += /*html*/ `
 				<div class="wrapper__bottom">
 					<ul class="response">
-						<li class="response__item"> <input type="checkbox"/></li>
+						<li class="response__item"> <input class="checkbox" type="checkbox"/></li>
 						<li class="response__item" style="display:none;">
 							<span>${data[i].id}</span>
 						</li>
 						<li class="response__item">
-							<span>${data[i].title.split('##')[0]}</span>
+							<input value= ${data[i].title.split('##')[0]} />
 						</li>
 						<li class="response__item">
-							<span>${data[i].title.split('##')[1]}</span>
+							<input type="date" value=${data[i].title.split('##')[1]} />
 						</li>
 						<li class="response__item">
 						<select class="status-input">
@@ -119,7 +150,6 @@ export default class TodoList extends Component {
 				</div>
 			`
 		}
-
 		const taskInput = this.el.querySelector('.task-input')
 		const dateInput = this.el.querySelector('.date-input')
 
@@ -128,11 +158,57 @@ export default class TodoList extends Component {
 			this.createTodo(taskInput, dateInput)
 		)
 
+		const statusInput = this.el
+			.querySelector('.status-input')
+			.addEventListener('keypress', function (e) {
+				if (e.key === 'Enter') {
+				}
+				console.log(statusInput)
+			})
+
 		const inputEl = this.el.querySelector('input')
 		inputEl.addEventListener('input', () => {
 			// store.state.todoText = inputEl.value
 		})
+		//항복 완료/미완료 분류//
+		// const taskStatus = this.el.querySelector('.status-input')
+		// taskStatus.sort((a, b) => {
+		// 	const arr = ['pending', 'completed']
+		// 	return arr.includes(a.status) || arr.includes(b.status) ? 1 : -1
+		// })
+		// console.log(taskStatus)
 
+		//input 수정하기//
+		const doubleClick = (e, todoId) => {
+			const todoElem = e.target
+			const inputText = e.target.innerText
+			const todoItemElem = todoElem.parentNode
+			const inputElem = document.createElement('input')
+			inputElem.value = inputText
+			inputElem.classList.add('edit-input')
+
+			inputElem.addEventListener('keypress', e => {
+				if (e.key === 'Enter') {
+					updateTodo(e.target.value, todoId) // todo 수정
+					document.body.removeEventListener('click', onClickBody) // 이벤트리스너 제거
+				}
+			})
+
+			todoItemElem.appendChild(inputElem) // todoItemElem 요소에 자식 요소로 inputElem 요소 추가
+
+			// body에 클릭에 대한 이벤트 리스너 등록
+			document.body.addEventListener('click', onClickBody)
+
+			// todoItemElem 요소를 제외한 영역을 클릭 시, 수정모드 종료
+			const onClickBody = e => {
+				if (e.target !== inputElem) {
+					todoItemElem.removeChild(inputElem)
+					document.body.removeEventListener('click', onClickBody)
+				}
+			}
+		}
+
+		//항목 삭제하기//
 		const deleteButtons = this.el.querySelectorAll('.delete')
 		deleteButtons.forEach(deleteButton => {
 			const todoId = deleteButton.parentNode.parentNode.children
